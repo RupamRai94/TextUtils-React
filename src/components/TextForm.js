a0fa8cb83dc7728b15d607938435effcfbf9a4cc
Rupam Rai <rrai838@gmail.com>
2022-12-03 19:51:03 -0600

    Improved dark mode and refactored app component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,26 +36,39 @@ export default function TextForm(props) {
         setText("")
     }
 
+    const handleCopyText = ()=>{
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    
+    const handleExtraSpaces = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
   return (
     <>
-        <div>
+        <div className="container" style={{color : props.mode === 'dark' ? 'white' : '#042743'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <label htmlFor="myBox" className="form-label">Example textarea</label>
                 {/* handleOnChange is using for change the state if someone tying on TB so it should change b/c of value={text} */}
-                <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="10"></textarea>
+                <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode === 'dark' ? 'grey' : 'white', 
+                color : props.mode === 'dark' ? 'white' : '#042743'}} rows="10"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-1" onClick={handleSwapCaseClick}>SwapCase</button>
             <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
+            <button className="btn btn-primary mx-1" onClick={handleCopyText}>Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color : props.mode === 'dark' ? 'white' : '#042743'}}>
             <h2>Your text summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length > 0 ? text : "Enter something in the above text area to preview it here"}</p>
         </div>
     </>
   )
